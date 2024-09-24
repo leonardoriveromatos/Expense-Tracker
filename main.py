234226ba1218d5e85dae62d06647ab3167351d03
Leonardo J. Rivero Matos <leonardojrmatos@gmail.com>
2024-09-24 15:35:51 -0400

    command add modified

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from datetime import datetime
 # Crear el parser
 parser = argparse.ArgumentParser(description="Expense Tracker CLI")
 
-parser.add_argument("-a","--add", help = "Create new expense", required = False)
+parser.add_argument("command", choices=["add", "remove", "update"])
+parser.add_argument("-d","--description", help = "Create new expense", required = False)
 parser.add_argument("-am","--amount", help = "Amount", required = False)
 parser.add_argument("-l","--list", action = "store_true", help = "List all expenses", required = False)
 
@@ -16,9 +17,9 @@ current_time = datetime.now().strftime("%H:%M")
 
 expenses = load()
 
-if args.add:
+if args.command == 'add':
     expense_id = len(expenses) + 1
-    new_expense = {"id": expense_id, "date": current_date, "time": current_time, "description": args.add, "amount": args.amount }
+    new_expense = {"id": expense_id, "date": current_date, "time": current_time, "description": args.description, "amount": args.amount }
     expenses.append(new_expense)
     save(expenses)
     print(f"Expense added successfully ID {expense_id}")
